b1f9f917ff701c0c07ce46a54f9e45b9b5cc5cfb
academeg1 <m-ignatovich@hotmail.com>
2022-09-19 17:00:47 +0300

    done task Handshake_problem.js

---
+++ Handshake_problem.js
@@ -0,0 +1,9 @@
+function getParticipants(handshakes){
+    let farmers = 0
+    while(handshakes > farmers * (farmers - 1) / 2){
+        farmers++
+    }
+    return farmers
+}
+
+console.log(getParticipants(1))
